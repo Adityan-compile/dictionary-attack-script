dfd9738edafe4c700c7925d0355fda0f9b408f33
Adityan-compile <67089770+Adityan-compile@users.noreply.github.com>
2020-09-26 15:21:39 +0530

    .

--- src/script.py
+++ src/script.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+
+__author__ = "https://github.com/Adityan-compile/"                             
+__copyright__ = "Copyright (c) 2020 Adityan-compile"
+__license__ = "MIT"
+__version__ = "1.0"
+
+
+import hashlib
 
